5c66c6abaf158a5c486f964964e931458b808da6
Franceslav <107437303+Franceslav@users.noreply.github.com>
2024-05-04 11:01:36 +0300

    коммит

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,8 @@
-import { Inter, Lusitana} from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
+import { Inter} from 'next/font/google';
+// fonts.ts
+import { Lusitana } from 'next/font/google';
 
 export const lusitana = Lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
+    weight: ['400', '700'],
+    subsets: ['latin'],
 });
